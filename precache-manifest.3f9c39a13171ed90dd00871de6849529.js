a52e7ae0b13383e3990bb077533cabe519800c8b
hemasri098 <hemasri.dangudubiyyam@zemosolabs.com>
2020-05-06 20:46:37 +0530

    Updates

---
+++ precache-manifest.3f9c39a13171ed90dd00871de6849529.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b860db5530908247bf1ebbc14918c6",
+    "url": "/example/index.html"
+  },
+  {
+    "revision": "b2f63b2ae57cc4d106f5",
+    "url": "/example/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b4d7026267d0003c0004",
+    "url": "/example/static/js/2.4f41af28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/example/static/js/2.4f41af28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f63b2ae57cc4d106f5",
+    "url": "/example/static/js/main.f86fa260.chunk.js"
+  },
+  {
+    "revision": "31c36a0f75f519140b20",
+    "url": "/example/static/js/runtime-main.4d220dbf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/example/static/media/logo.5d5d9eef.svg"
+  }
+]);
